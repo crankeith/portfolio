3e389c5236b36e3c9db9a32ed685fd1267f18984
Andrew Keith <38077188+crankeith@users.noreply.github.com>
2019-05-12 10:43:53 -0400

    First version

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Development Portfolio
 
+## Project
 This project is a express/node application that serves a portfolio of projects I've completed throughout my Techdegree a Treehouse (http://teamtreehouse.com).
+
+###Extra Credit
+
+- I've set up my package.json to utilize npm start and test. See development notes below.
+- I've added error handling for the following scenarios:
+    - Project route sends back a 400 error if project id in path doesn't exist
+    - Project route sends back a 500 error if the data.json file has duplicate project ids.
+    - If a route doesn't exist, a 404 error is flagged
+- I've update the css and changed the following: 
+    - Background color of the left side panel
+    - Profile image now has a border radius
+    - Font colors changed in buttons, links and left panel
+    - Font changed to Open Sans
+
+##Development
+###npm commands
+`npm start` - Runs nodemon to start the application.
+
+`npm test` - Runs nodemon with --inspect flag for chrome dev tools usage. 
--- app.js
+++ app.js
@@ -1,10 +1,67 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const data = require('./data.json');
+const favicon = require('serve-favicon');
+
+
 app.use('/static', express.static('public'));
+app.use(favicon(path.join(__dirname, 'public/img', 'favicon.png')));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('layout');
+    printMessage(200, `/`);
+    res.locals.projects = data.projects;
+    res.render('index');
 });
 
-app.listen(3001);
+app.get('/about', (req, res) => {
+    printMessage(200, `/about`);
+    res.render('about');
+});
+
+app.get('/projects/:id', (req, res, next) => {
+    const projectId = Number(req.params.id);
+
+    const selectProjects = data.projects.filter(project => project.id === projectId);
+    if(selectProjects.length === 1){
+        //Success
+        res.locals.project = selectProjects[0];
+        printMessage(200, `/projects/${projectId}`);
+        res.render('project');
+    } else if (selectProjects.length > 1) {
+        //Duplicate project ids
+        const err = new Error('Error in data. Duplicate project IDs');
+        err.status = 500;
+        next(err);
+    } else {
+        //Project doesn't exist
+        const err = new Error('Project does not exist');
+        err.status = 400;
+        next(err);
+    }
+});
+
+//Handle 404 not found
+app.use((req,res, next)=>{
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//Handle errors
+app.use((err, req, res, next)=>{
+    res.locals.error = err;
+    printMessage(err.status, err.message, true);
+    res.render('error');
+});
+
+app.listen(3000);
+
+function printMessage(code, message, isError){
+    if(!isError){
+        console.log(code, message);
+    } else {
+        console.error(code, message);
+    }
+}
